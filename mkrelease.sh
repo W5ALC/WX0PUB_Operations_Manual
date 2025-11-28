e4e041acff0ce7c4e70ecb5639ec0a90c3ab06ad
Jon Poindexter <7112421+W5ALC@users.noreply.github.com>
2025-11-27 22:43:16 -0700

    Add mkrelease.sh for building and zipping manuals
    Script to compile LaTeX and create a zip of manuals.

---
+++ mkrelease.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pdflatex Pueblo_SKYWARN_Ops_Manual.tex
+#pdflatex N4VEM_Ops_Manual.tex
+if [[ -z "$1" ]]; then
+	echo Release version string not specified
+	echo Usage:  $0 1.0.2-rc2
+else
+	zip -9o "Pueblo_SKYWARN_Ops_Manual-$1.zip" *.pdf *.txt
+fi
